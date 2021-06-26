b4577bb6a9efa9a7819a8a9305f0db77b96c210b
Conrad Bekondo <philllittle302@gmail.com>
2021-06-26 01:11:05 +0100

    initial commit

---
+++ GozemApi/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace GozemApi.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
